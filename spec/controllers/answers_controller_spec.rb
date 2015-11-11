230e9683383a8dfd914768fc84a80edd84e0ff3e
maximsolopin <maxim.solopin@gmail.com>
2015-11-12 01:42:45 +0300

    added controllers tests for answers

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,126 @@
+require 'rails_helper'
+
+describe AnswersController do
+  let (:question) { create(:question) }
+  let (:answer) { create(:answer, question: question) }
+
+  describe 'GET #index' do
+    let(:answers) { create_list(:answer, 2, question: question) }
+
+    before { get :index, question_id: question }
+
+    it 'populates an array of all answers' do
+      expect(assigns(:answers)).to match_array(answers)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, id: answer, question_id: question }
+
+    it 'assigns the requested answer to @answer' do
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new, question_id: question }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, id: answer, question_id: question }
+
+    it 'assigns the requested answer to @answer' do
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new answer in the database' do
+        expect { post :create, answer: attributes_for(:answer), question_id: question }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, answer: attributes_for(:answer), question_id: question
+        expect(response).to redirect_to answer_path(assigns(:answer))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer in the database' do
+        expect { post :create, answer: attributes_for(:invalid_answer), question_id: question }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, answer: attributes_for(:invalid_answer), question_id: question
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'valid attributes' do
+      it 'assigns the requested answer to @answer' do
+        patch :update, id: answer, answer: attributes_for(:answer), question_id: question
+        expect(assigns(:answer)).to eq answer
+      end
+
+      it 'change answer attributes' do
+        patch :update, id: answer, answer: { body: 'new body' }, question_id: question
+        answer.reload
+        expect(answer.body).to eq 'new body'
+      end
+
+      it 'redirects to the updated answer' do
+        patch :update, id: answer, answer: attributes_for(:answer), question_id: question
+        expect(response).to redirect_to answer
+      end
+    end
+
+    context 'invalid attributes' do
+      before { patch :update, id: answer, answer: { body: nil }, question_id: question }
+
+      it 'does not change answer attributes' do
+        answer.reload
+        expect(answer.body).to eq 'MyString'
+      end
+
+      it 're-renders edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before { answer }
+
+    it 'deletes answer' do
+      expect { delete :destroy, id: answer, question_id: question }.to change(Answer, :count).by(-1)
+    end
+
+    it 'redirect to index view' do
+      delete :destroy, id: answer, question_id: question
+      expect(response).to redirect_to question_answers_path(question)
+    end
+  end
+end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :answer do
+    question
     body "MyString"
   end
 
+  factory :invalid_answer, class: "Answer"  do
+    question
+    body ""
+  end
 end
