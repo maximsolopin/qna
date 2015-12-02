d5b2f3c4831d3add6461f71929274ebb28df8747
maximsolopin <maxim.solopin@gmail.com>
2015-12-02 16:47:23 +0300

    Polymorphic attachments. Answers associated with attachments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,13 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+  has_many :attachments, as: :attachable, dependent: :destroy
 
   validates :body, presence: true
   validates :user_id, presence: true
   validates :question_id, presence: true
+
+  accepts_nested_attributes_for :attachments
 
   default_scope { order('best desc','created_at') }
 
