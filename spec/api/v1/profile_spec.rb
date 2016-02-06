e2a0bf2a8fc61ddb165358225517ea7f1e5b5f0b
mirsanova <kseniya.solopina@gmail.com>
2016-02-06 23:07:13 +0300

    lesson 15

--- spec/api/v1/profile_spec.rb
+++ spec/api/v1/profile_spec.rb
@@ -55,6 +55,7 @@ describe 'Profile API' do
       let(:me) { create :user }
       let!(:other_users) { create_list(:user, 10) }
       let(:access_token) { create :access_token, resource_owner_id: me.id }
+      let(:user) { user = other_users.first }
 
       before { get '/api/v1/profiles', format: :json, access_token: access_token.token }
 
@@ -63,11 +64,23 @@ describe 'Profile API' do
       end
 
       it 'contains users' do
-        expect(response.body).to be_json_eql(other_users.to_json)
+        expect(response.body).to be_json_eql(other_users.to_json).at_path("profiles")
       end
 
       it 'does not contain me' do
         expect(response.body).to_not include_json me.to_json
+      end
+
+      %w(id email created_at updated_at admin).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(user.send(attr.to_sym).to_json).at_path("profiles/0/#{attr}")
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "does not contain #{attr}" do
+          expect(response.body).to_not have_json_path("profiles/0/#{attr}")
+        end
       end
     end
   end
